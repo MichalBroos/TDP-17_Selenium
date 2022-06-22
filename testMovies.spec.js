50c5b55eb72a5ea1d3835c1c76ef0493cfd320a2
Michal Broos <2330994b@gmail.com>
2022-06-22 16:32:50 +0100

    Add 3 getAll link for Movies FE+BE

--- testMovies.spec.js
+++ testMovies.spec.js
@@ -14,7 +14,37 @@ describe("Test Movies FE", function () {
 
     it("should have the correct h1 on /", async () => {
         await driver.get("http://localhost:3000/");
-        const h1 = await driver.findElement(By.xpath("/html/body/div/h1")).getText();
-        assert.equal(h1, "Movies FE + BE");
+        const homeH1 = await driver.findElement(By.xpath("/html/body/div/h1")).getText();
+        assert.equal(homeH1, "Movies FE + BE");
+    });
+
+    it("getAll link from / stays on /", async () => {
+        await driver.get("http://localhost:3000/");
+        const url1 = await driver.getCurrentUrl()
+        // getAll link in navbar
+        await driver.findElement(By.xpath("/html/body/div/a[1]")).click();
+        const url2 = await driver.getCurrentUrl()
+        console.log(url1, "?", url2);
+        assert.equal(url1, url2);
+    });
+
+    it("getAll link from /add does not stay on /add", async () => {
+        await driver.get("http://localhost:3000/add");
+        const url1 = await driver.getCurrentUrl()
+        // getAll link in navbar
+        await driver.findElement(By.xpath("/html/body/div/a[1]")).click();
+        const url2 = await driver.getCurrentUrl()
+        console.log(url1, "?", url2);
+        assert.notEqual(url1, url2);
+    });
+
+    it("getAll link from /getbyid ends on /", async () => {
+        await driver.get("http://localhost:3000/getbyid");
+        const url1 = "http://localhost:3000/"
+        // getAll link in navbar
+        await driver.findElement(By.xpath("/html/body/div/a[1]")).click();
+        const url2 = await driver.getCurrentUrl()
+        console.log(url1, "?", url2);
+        assert.equal(url1, url2);
     });
 });
